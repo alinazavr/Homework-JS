520bb3fa81edd80c9ce9517b7158d41b8b5bf1fb
alinazavr <56886836+alinazavr@users.noreply.github.com>
2019-11-20 00:13:50 +0300

    HW1

--- HW1/js/program.js
+++ HW1/js/program.js
@@ -36,10 +36,28 @@ console.log(minus1)
 var minus2 = 10 - n 
 console.log(minus2)
 var res = 10 - Math.min(minus1, minus2)
-console.log('Число, ближайшее к 10 - ' + res)*/
-/*
+console.log('Число, ближайшее к 10 - ' + res)
+
+
 // 5 задача
 let X = [0, 0, 0],
     Y = [5, 0, 0],
     Z = [0, 0, 7],
-    XY = 
+    XY, YZ, ZX
+    XY = Math.sqrt(Math.pow(Y[0] - X[0], 2) + Math.pow(Y[1] - X[1], 2) + Math.pow(Y[2] - X[2], 2))
+    YZ = Math.sqrt(Math.pow(Z[0] - Y[0], 2) + Math.pow(Z[1] - Y[1], 2) + Math.pow(Z[2] - Y[2], 2))
+    ZX = Math.sqrt(Math.pow(X[0] - Z[0], 2) + Math.pow(X[1] - Z[1], 2) + Math.pow(X[2] - Z[2], 2))
+
+if (Math.pow(XY) + Math.pow(YZ) === Math.pow(ZX)) {
+    console.log('Треуголник прямоугольный')
+
+} else if (Math.pow(XY) + Math.pow(ZX) === Math.pow(YZ)) {
+    console.log('Треуголник прямоугольный')
+
+} else if (Math.pow(YZ) + Math.pow(ZX) === Math.pow(XY)) {
+    console.log('Треуголник прямоугольный')
+}
+else {
+    console.log('Треугольник непрямоугольный')
+}
+
