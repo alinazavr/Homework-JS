3be84c280f23053ee5615e007b86028cf6ca9b28
alinazavr <56886836+alinazavr@users.noreply.github.com>
2019-11-21 01:51:58 +0300

    ispravleno

--- HW1/js/program.js
+++ HW1/js/program.js
@@ -1,5 +1,5 @@
 "use strict"
-
+/*
 // 1 задача
 var square = 10
 var width = 25
@@ -25,18 +25,21 @@ var number3 = prompt('Введите третье число')
 console.log(number3)  
 var res = Math.min(number1, number2, number3)
 console.log('Минимальное число ' + res)
-
+*/
 // 4 задача
 var n = prompt('Введите первое число')
 console.log(n)
 var m = prompt('Введите второе число')
 console.log(m)
-var minus1 = 10 - m 
-console.log(minus1)
-var minus2 = 10 - n 
-console.log(minus2)
-var res = 10 - Math.min(minus1, minus2)
-console.log('Число, ближайшее к 10 - ' + res)
+var minus1 = Math.abs(10 - m),
+    minus2 = Math.abs(10 - n)
+if (minus1 > minus2) {
+    console.log('Ближайшее к 10 - ' + n)
+} else if (minus2 > minus1) {
+    console.log('Ближайшее к 10 - ' + m)
+} else {
+    console.log('Числа равноудалены от 10')
+}   
 
 
 // 5 задача
@@ -44,17 +47,17 @@ let X = [0, 0, 0],
     Y = [5, 0, 0],
     Z = [0, 0, 7],
     XY, YZ, ZX
-    XY = Math.sqrt(Math.pow(Y[0] - X[0], 2) + Math.pow(Y[1] - X[1], 2) + Math.pow(Y[2] - X[2], 2))
-    YZ = Math.sqrt(Math.pow(Z[0] - Y[0], 2) + Math.pow(Z[1] - Y[1], 2) + Math.pow(Z[2] - Y[2], 2))
-    ZX = Math.sqrt(Math.pow(X[0] - Z[0], 2) + Math.pow(X[1] - Z[1], 2) + Math.pow(X[2] - Z[2], 2))
+    XY = Math.pow(Y[0] - X[0], 2) + Math.pow(Y[1] - X[1], 2) + Math.pow(Y[2] - X[2], 2)
+    YZ = Math.pow(Z[0] - Y[0], 2) + Math.pow(Z[1] - Y[1], 2) + Math.pow(Z[2] - Y[2], 2)
+    ZX = Math.pow(X[0] - Z[0], 2) + Math.pow(X[1] - Z[1], 2) + Math.pow(X[2] - Z[2], 2)
 
-if (Math.pow(XY) + Math.pow(YZ) === Math.pow(ZX)) {
+if (XY + YZ === ZX) {
     console.log('Треуголник прямоугольный')
 
-} else if (Math.pow(XY) + Math.pow(ZX) === Math.pow(YZ)) {
+} else if (XY + ZX === YZ) {
     console.log('Треуголник прямоугольный')
 
-} else if (Math.pow(YZ) + Math.pow(ZX) === Math.pow(XY)) {
+} else if (YZ + ZX === XY) {
     console.log('Треуголник прямоугольный')
 }
 else {
